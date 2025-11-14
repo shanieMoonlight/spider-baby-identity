36f3765d4199738136b899225205117777c0c86d
shanebrannick <shanebrannick@yahoo.com>
2025-11-14 18:55:01 +0000

    IdGlobalConstants.Jobs

--- Identity/Infrastructure/ID.Jobs.Quartz/Setup.cs
+++ Identity/Infrastructure/ID.Jobs.Quartz/Setup.cs
@@ -5,7 +5,7 @@ using ID.Application.Models;
 using ID.Domain.Entities.Teams;
 using ID.Jobs.Quartz.AppImps;
 using ID.Jobs.Quartz.Persistence;
-using ID.Jobs.Quartz.Persistence.MigrationNotifications;
+using ID.GlobalSettings.Constants;
 using ID.Jobs.Quartz.Retries;
 using ID.Jobs.Quartz.Servers;
 using Microsoft.AspNetCore.Builder;
@@ -61,19 +61,19 @@ public static class Setup
         switch (minTeamTypeDashboardAccess)
         {
             case TeamType.super:
-                app.UseExternalPagesAuth_SuperTeam("/myid-jobs-dashboard");
+                app.UseExternalPagesAuth_SuperTeam(IdGlobalConstants.Jobs.DashboardPath);
                 break;
             case TeamType.maintenance:
-                app.UseExternalPagesAuth_MntcMinimum("/myid-jobs-dashboard");
+                app.UseExternalPagesAuth_MntcMinimum(IdGlobalConstants.Jobs.DashboardPath);
                 break;
             case TeamType.customer:
-                app.UseExternalPagesAuth_CustomerMinimum("/myid-jobs-dashboard");
+                app.UseExternalPagesAuth_CustomerMinimum(IdGlobalConstants.Jobs.DashboardPath);
                 break;
         }
 
 
         app.UseCrystalQuartz(() => app.ApplicationServices.GetRequiredService<ISchedulerFactory>().GetScheduler(QuartzConstants.Scheduler),
-            new CrystalQuartzOptions { Path = "/myid-jobs-dashboard" }
+            new CrystalQuartzOptions { Path = IdGlobalConstants.Jobs.DashboardPath }
         );
 
         return app;
